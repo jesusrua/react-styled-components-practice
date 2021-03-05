49393962682118a854d24a59dd7793f02614df4f
Jesus Rua <jesus.rua.pareja@gmail.com>
2021-03-05 20:14:28 +0100

    react-styled-components

---
+++ src/components/Products/data.js
@@ -0,0 +1,69 @@
+import product1 from '../../Images/product1.jpg'
+import product2 from '../../Images/product2.jpg'
+import product3 from '../../Images/product3.jpg'
+import product4 from '../../Images/product4.jpg'
+import sweet1 from '../../Images/crepeNutella.jpeg'
+import sweet2 from '../../Images/tiramisu.jpeg'
+import sweet3 from '../../Images/gelatto.jpeg'
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, ...',
+    price: '19,99€',
+    button: 'Add to Cart'
+  },
+    {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, ...',
+    price: '19,99€',
+    button: 'Add to Cart'
+  },
+    {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, ...',
+    price: '19,99€',
+    button: 'Add to Cart'
+  },
+    {
+    img: product4,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, ...',
+    price: '19,99€',
+    button: 'Add to Cart'
+  }
+]
+
+export const productDataTwo = [
+  {
+    img: sweet1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, ...',
+    price: '19,99€',
+    button: 'Add to Cart'
+  },
+    {
+    img: sweet2,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, ...',
+    price: '19,99€',
+    button: 'Add to Cart'
+  },
+    {
+    img: sweet3,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, ...',
+    price: '19,99€',
+    button: 'Add to Cart'
+  }
+]
